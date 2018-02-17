adada0c704e54dbbdff091ae808be42b918b98db
John Blaine <jablaine@uw.edu>
2018-02-17 08:05:28 -0800

    Create algorithm for NPV and build out front-end

---
+++ client/src/components/helpers/calculators.js
@@ -0,0 +1,6 @@
+var netPresentValue = function(initialInvestment,cashInflows,  numberOfPeriods, discountRate) {
+
+  return (cashInflows * ((1 - (Math.pow((1+discountRate), -numberOfPeriods))) / discountRate) - initialInvestment);
+};
+
+module.exports = netPresentValue;
